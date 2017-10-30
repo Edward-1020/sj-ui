1afee71ac60e338eb12573c5d820ee381ac9106b
young <yangxu@yeezon.com>
2017-10-30 18:55:57 +0800

    fix modal css

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <sj-button bottom-action = "true" type="primary">aaa</sj-button>
-    <sj-loading/>
+    <sj-button bottom-action = "true" type="primary" @click.native="show = !show">click</sj-button>
+    <transition name="fade">
+      <sj-button type="primary" v-if="show">aaaaa</sj-button>
+    </transition>
   </div>
 </template>
 
 <script>
 import Button from './package/button'
-import Loading from './package/loading'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      show: true
+    }
+  },
   components: {
-    [Button.name]: Button,
-    [Loading.name]: Loading
+    [Button.name]: Button
   }
 }
 </script>
 
 <style>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity 5s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
 </style>
