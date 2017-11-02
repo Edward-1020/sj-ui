d1997b98e4223521b2dd5822d4eb84e4401eba53
young <yangxu@yeezon.com>
2017-11-02 19:26:44 +0800

    init badge-group

---
+++ src/package/badge-group/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="sj-badge-group sj-hairline--top-bottom">
+    <slot></slot>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'sj-badge-group',
+
+  props: {
+    activeKey: {
+      type: [Number, String],
+      default: 0
+    }
+  },
+
+  data () {
+    return {
+      badges: []
+    }
+  }
+}
+</script>
+<style>
+</style>
