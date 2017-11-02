6872ab45269063b4ae9099240b8ba5ce11cfaa3c
young <yangxu@yeezon.com>
2017-11-02 19:12:29 +0800

    init badge

---
+++ src/package/badge/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <a :class="['sj-badge sj-hairline', {'sj-badge--select': isSelect}]" :href="url" @click="onClick">
+    <div v-if="info" class="sj-badge__info">{{info}}</div>
+    {{title}}
+  </a>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'sj-badge',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      default: 'javascript:;'
+    },
+    info: {
+      type: String
+    }
+  },
+
+  beforeCreate () {
+    this.$parent.badges.push(this)
+  },
+
+  computed: {
+    isSelect () {
+      const parent = this.$parent
+      return parent.badges.indexOf(this) === parent.activeKey
+    }
+  },
+
+  methods: {
+    onClick () {
+      this.$emit('click', this.$parent.badges.indexOf(this))
+    }
+  }
+}
+</script>
+<style>
+</style>
